7266e820ac6fdc287209d365d7aa46a81fc31c68
Sean Ediger <Ediger.s@northeastern.edu>
2024-01-23 15:15:55 -0500

    seg_url update

--- import_segments.py
+++ import_segments.py
@@ -1,3 +1,11 @@
 
 from intern import array
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+seg_url = ""
+
+segments = array(seg_url)
+
+
+print(segments.shape)
+print(segments.dtype)
